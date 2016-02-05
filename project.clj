07e09ab2c2d4de476e486a67007ac9789d1ebc47
Pedro Santos <donbonifacio@gmail.com>
2016-02-05 15:48:47 +0000

    Config if days off should be considered, by absence config
    Until now, we would remove days off (weekends, holidays, ...) from the
    total days of vacations. Now that can be configured on absence configs.
    Each absence config can have a consider-days-off boolean that handles
    this condition.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/work-days "0.6.0"
+(defproject clanhr/work-days "0.7.0"
   :description "Work days calculation"
   :url "https://github.com/clanhr/work-days"
   :dependencies [[org.clojure/clojure "1.8.0"]
