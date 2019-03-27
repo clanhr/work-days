d5fafc42840ff1743445393c599c2f6490d7ac57
Paulo Silva <paulosilva86@gmail.com>
2019-03-27 15:11:10 +0000

    Consider hours for a half-day absence
    Why:
    
    * Absences (besides "vacations") are always counted in hours.
    
    This change addresses the need by:
    
    * Calculating a half-day in hours based on the hours of a total-day.
    
    Notes:
    
    * By default a total-day is 8 hours, hence a half-day will be 4 hours.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/work-days "0.10.0"
+(defproject clanhr/work-days "0.10.1"
   :description "Work days calculation"
   :url "https://github.com/clanhr/work-days"
   :dependencies [[org.clojure/clojure "1.8.0"]
